79015724b0b1fbba3ac93d237b6b62b51ab6932f
BibenGurung <109825370+BibenGurung@users.noreply.github.com>
2023-03-07 15:37:16 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+//https://teachablemachine.withgoogle.com/models/waqsieZIM/
+
+Webcam.set({
+    width:350,
+    height:300,
+    image_format : 'png',
+    png_quality:90
+});
+camera = document.getElementById("camera");
+
+Webcam.attach( '#camera');
+
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
+    });
+}
+
+console.log('ml5 version:', ml5.version);
+
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/waqsieZIM/ model.jsn',modelLoaded);
+
+function modelLoaded()
+{
+    console.log("modelLoaded")
+}
